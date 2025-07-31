92867afe917b4a5d65d44ab844699cc9985766c9
miramab <mirzamab800@gmail.com>
2025-07-31 16:15:44 +0530

    Upadte secret code

--- src/components/SecretCodeCard.jsx
+++ src/components/SecretCodeCard.jsx
@@ -19,7 +19,7 @@ export default function SecretCodeCard({ onCorrect }) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (code === '03082023') {
+        if (code === '382023') {
             onCorrect()
         } else {
             setError("Oops! That’s not the right code.")
